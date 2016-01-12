b0e81b5cb67ebe929ce500cb890198e47e636f22
linux_china <linux_china@hotmail.com>
2016-01-12 17:26:57 +0800

    togglz operation endpoint

---
+++ src/main/java/org/mvnsearch/spring/boot/togglz/TogglzOperationEndpoint.java
@@ -0,0 +1,50 @@
+package org.mvnsearch.spring.boot.togglz;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.Endpoint;
+import org.springframework.boot.actuate.endpoint.mvc.MvcEndpoint;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * togglz operation endpoint
+ *
+ * @author leijuan
+ */
+@Controller
+public class TogglzOperationEndpoint implements MvcEndpoint {
+    @Autowired
+    private TogglzProperties properties;
+
+    @RequestMapping(value = "/enable/{feature}", method = RequestMethod.GET)
+    @ResponseBody()
+    public String enable(@PathVariable String feature) {
+        properties.getFeatures().put(feature, true);
+        return "success";
+    }
+
+    @RequestMapping("/disable/{feature}")
+    @ResponseBody()
+    public String disable(@PathVariable String feature) {
+        properties.getFeatures().put(feature, false);
+        return "success";
+    }
+
+    public String getPath() {
+        return "switch";
+    }
+
+    public boolean isSensitive() {
+        return false;
+    }
+
+    public Class<? extends Endpoint> getEndpointType() {
+        return null;
+    }
+}
