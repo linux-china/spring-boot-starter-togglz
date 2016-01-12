e296e0a1cd8f8f337cfb629e711661d2154f0e25
linux_china <linux_china@hotmail.com>
2016-01-12 17:27:13 +0800

    add operation

--- src/main/java/org/mvnsearch/spring/boot/togglz/TogglzAutoConfiguration.java
+++ src/main/java/org/mvnsearch/spring/boot/togglz/TogglzAutoConfiguration.java
@@ -1,6 +1,7 @@
 package org.mvnsearch.spring.boot.togglz;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +10,10 @@ import org.togglz.core.Feature;
 import org.togglz.core.context.StaticFeatureManagerProvider;
 import org.togglz.core.manager.FeatureManager;
 import org.togglz.core.manager.FeatureManagerBuilder;
+import org.togglz.core.manager.TogglzConfig;
 import org.togglz.core.repository.StateRepository;
 import org.togglz.core.user.NoOpUserProvider;
+import org.togglz.core.user.UserProvider;
 
 /**
  * togglz auto configuration
@@ -26,21 +29,45 @@ public class TogglzAutoConfiguration {
     private TogglzProperties properties;
 
     @Bean
-    public FeatureManager featureManager() {
+    @ConditionalOnMissingBean
+    public FeatureManager featureManager(TogglzConfig togglzConfig) {
         FeatureManagerBuilder builder = new FeatureManagerBuilder();
-        StateRepository stateRepository = new EnvironmentStateRepository(properties);
         Class<Feature>[] clazzList = new Class[properties.getFeatureEnum().size()];
-        builder.featureEnums(properties.getFeatureEnum().toArray(clazzList))
-                .stateRepository(stateRepository)
-                .userProvider(new NoOpUserProvider());
+        builder.featureEnums(properties.getFeatureEnum().toArray(clazzList)).togglzConfig(togglzConfig);
         FeatureManager featureManager = builder.build();
         StaticFeatureManagerProvider.setFeatureManager(featureManager);
         return featureManager;
     }
 
     @Bean
+    @ConditionalOnMissingBean
+    public TogglzConfig togglzConfig() {
+        return new TogglzConfig() {
+            @Override
+            public Class<? extends Feature> getFeatureClass() {
+                return Feature.class;
+            }
+
+            @Override
+            public StateRepository getStateRepository() {
+                return new EnvironmentStateRepository(properties);
+            }
+
+            @Override
+            public UserProvider getUserProvider() {
+                return new NoOpUserProvider();
+            }
+        };
+    }
+
+    @Bean
+    public TogglzOperationEndpoint switchOperationEndpoint() {
+        return new TogglzOperationEndpoint();
+    }
+
+    @Bean
     public TogglzEndpoint togglzEndpoint(TogglzProperties properties, FeatureManager featureManager) {
-        return new TogglzEndpoint(properties, featureManager);
+        return new TogglzEndpoint(properties);
     }
 
 }
