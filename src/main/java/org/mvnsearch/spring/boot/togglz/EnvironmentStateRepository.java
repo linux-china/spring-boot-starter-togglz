1234868b41467eeed58d3059aec78badc7bbce14
linux_china <linux_china@hotmail.com>
2016-01-12 10:17:32 +0800

    environment state repository

---
+++ src/main/java/org/mvnsearch/spring/boot/togglz/EnvironmentStateRepository.java
@@ -0,0 +1,27 @@
+package org.mvnsearch.spring.boot.togglz;
+
+import org.togglz.core.Feature;
+import org.togglz.core.repository.FeatureState;
+import org.togglz.core.repository.StateRepository;
+
+/**
+ * environment state repository
+ *
+ * @author linux_china
+ */
+public class EnvironmentStateRepository implements StateRepository {
+    private TogglzProperties properties;
+
+    public EnvironmentStateRepository(TogglzProperties togglzProperties) {
+        this.properties = togglzProperties;
+    }
+
+    public FeatureState getFeatureState(Feature feature) {
+        return new EnvironmentFeatureState(feature, properties);
+    }
+
+    public void setFeatureState(FeatureState featureState) {
+        String name = featureState.getFeature().name();
+        properties.getFeatures().put(name, featureState.isEnabled());
+    }
+}
